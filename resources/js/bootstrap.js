a38d6b6e489dfcf19f74dc4aee914df152e741c1
CJF <carlitos991961@hotmail.com>
2023-04-24 21:11:55 +0200

    Actualizado

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -7,6 +7,9 @@
 import axios from 'axios';
 window.axios = axios;
 
+import '../css/app.css'
+import * as bootstrap from 'bootstrap'
+
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,21 @@
+<footer class="bg-dark text-white">
+    <div class="container py-3">
+      <div class="row">
+        <div class="col-md-6">
+          <h5>Contacto</h5>
+          <p>Teléfono: </p>
+          <p>Email: </p>
+        </div>
+        <div class="col-md-6">
+          <h5>Enlaces</h5>
+          <ul class="list-unstyled">
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Nosotros</a></li>
+            <li><a href="#">Productos</a></li>
+            <li><a href="#">Contacto</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+  
